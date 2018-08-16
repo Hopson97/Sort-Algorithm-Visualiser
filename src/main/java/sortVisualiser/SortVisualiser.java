f0f8447149198335c26ff782bc1e9844e989d19e
Hopson97 <mhopson@hotmail.co.uk>
2018-08-16 12:57:26 +0100

    Have JFrame open up and that

--- src/main/java/sortVisualiser/SortVisualiser.java
+++ src/main/java/sortVisualiser/SortVisualiser.java
@@ -7,11 +7,16 @@ import javax.swing.JFrame;
  * @author mhops
  */
 public class SortVisualiser {
+    public static final int WIN_WIDTH = 1280;
+    public static final int WIN_HEIGHT = 720;
     
     private JFrame window;
     
     public SortVisualiser() {
         window = new JFrame("Sort Visualiser");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(WIN_WIDTH, WIN_HEIGHT);
+        window.setVisible(true);
     }
     
     public static void main(String... args) {
