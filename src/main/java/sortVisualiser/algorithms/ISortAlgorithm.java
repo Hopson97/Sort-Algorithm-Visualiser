4ca59a110a6afd0c6e54bb83f11b08e1809eac2b
Wabri <gabriele.puliti@gmail.com>
2018-09-04 20:28:57 +0200

    formatting things

--- src/main/java/sortVisualiser/algorithms/ISortAlgorithm.java
+++ src/main/java/sortVisualiser/algorithms/ISortAlgorithm.java
@@ -4,11 +4,13 @@ import sortVisualiser.SortArray;
 
 /**
  * Base interface for the sort algorithms
+ *
  * @author Matt Hopson
  */
-public interface ISortAlgorithm 
-{
+public interface ISortAlgorithm {
     public String getName();
+
     public long getDelay();
+
     public void runSort(SortArray array);
 }
