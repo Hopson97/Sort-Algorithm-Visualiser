df278058649a0c15e2a968af1f03007999308892
Wabri <gabriele.puliti@gmail.com>
2018-09-04 20:27:41 +0200

    created javadocs for selection sort

--- src/main/java/sortVisualiser/algorithms/SelectionSort.java
+++ src/main/java/sortVisualiser/algorithms/SelectionSort.java
@@ -4,10 +4,21 @@ import sortVisualiser.SortArray;
 
 /**
  * Selection sort implementation
+ *
  * @author Matt Hopson
  */
-public class SelectionSort implements ISortAlgorithm
-{
+public class SelectionSort implements ISortAlgorithm {
+
+    /**
+     * This method implements the Selection sort algorithm, see
+     * <a href="https://en.wikipedia.org/wiki/Selection_sort">Selection_sort</a> to understand more.
+     * Takes a SortArray object called array and sorts his elements according to the mathematical theory
+     * of the order "less than", see <a href="https://en.wikipedia.org/wiki/Order_theory">Order_theory</a> to
+     * understand more.
+     *
+     * @param array the array to be sorted
+     * @see SortArray
+     */
     @Override
     public void runSort(SortArray array) {
         int len = array.arraySize();
